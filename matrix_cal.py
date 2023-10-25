c3432d21d22ae837f2c35b18e9acac2b1c6621d5
M Nisar <mahammadn143@gmail.com>
2023-10-25 11:29:03 +0530

    difference done

--- matrix_cal.py
+++ matrix_cal.py
@@ -1,30 +1,19 @@
 import pandas as pd
 
-from metrix_util import get_d , get_query , calculate_mat , get_query_pr
+from metrix_util import get_d , get_query , calculate_mat , get_query_pr , sum_up_down
 
-kianase = 'PAK1'
+kianase = 'PRPF4B'
 
-# upregulated 
-expression = 'Up-regulated'
-q = get_query(kianase,expression)
+q = get_query(kianase)
 df = get_d(q)
-mdf = calculate_mat(df)
-mdf.to_excel(f"{kianase}_site_matrix{expression}.xlsx")
-print("differentil upregulated done")
-# down-regulated
-
-expression = 'down-regulated'
-q = get_query(kianase,expression)
-df = get_d(q)
-mdf = calculate_mat(df)
-mdf.to_excel(f"{kianase}_site_matrix{expression}.xlsx")
+mdf_down = calculate_mat(df)
+mdf_down.to_excel(f"{kianase}_site_matrix.xlsx")
 print("differentil down-regulated done")
 
 
-
 # profile
-q = get_query_pr(kianase)
-df = get_d(q)
-mdf = calculate_mat(df)
-mdf.to_excel(f"{kianase}_Profile_site_matrix.xlsx")
-print("Profile done")
+# q = get_query_pr(kianase)
+# df = get_d(q)
+# mdf = calculate_mat(df)
+# mdf.to_excel(f"{kianase}_Profile_site_matrix.xlsx")
+# print("Profile done")
--- metrix_util.py
+++ metrix_util.py
@@ -2,8 +2,8 @@ import pymysql
 import pandas as pd
 
 
-def get_query(k, expression):
-    q = f'''SELECT exp_condition,mapped_phosphosite,expression FROM  phospodb_nisar_differential_data WHERE mapped_genesymbol = '{k}' AND expression='{expression}' '''
+def get_query(k):
+    q = f'''SELECT exp_condition,mapped_phosphosite,expression FROM  phospodb_nisar_differential_data WHERE mapped_genesymbol = '{k}' '''
     return q
 
 
@@ -27,26 +27,91 @@ def get_d(query):
     connection.close()
     return df_result
 
-def getcount(exp_c , exp_loc):
-    return sum(x in exp_c for x in exp_loc)
+def getcount_up(exp_c , exp_loc):
+    count_up = 0
+    count_down = 0
+    for k,v in exp_loc.items():
+        if v == "Up-regulated":
+            for ke, ve in exp_c.items():
+                if (k == ke ) and (ve == v):
+                    count_up += 1 
+        else:
+            for ke, ve in exp_c.items():
+                if (k == ke ) and (ve == v):
+                    count_down += 1 
+
+    return count_up , count_down
+
+    # return sum(x in exp_c for k,v in exp_loc if )
+
+def combine(cond, exp):
+    return dict(zip(cond, exp))
+
+
 
 def calculate_mat(df):
+
     df = df.groupby('mapped_phosphosite').agg(pd.Series.tolist).reset_index()
     df['count'] = df["exp_condition"].apply(lambda x:len(x))
+
+    df['up_count'] = df["expression"].apply(lambda x:x.count("Up-regulated"))
+    df['down_count'] = df["expression"].apply(lambda x:x.count("down-regulated")) 
+
     df = df.sort_values(by=['count'], ascending=False)
+
+
     all_sites = df['mapped_phosphosite']
 
-    df = df[['mapped_phosphosite','exp_condition','count']]
+    df['condition_exp'] = df.apply(lambda x:combine(x["exp_condition"],x["expression"]) , axis = 1) 
+
+    df = df[['mapped_phosphosite','condition_exp']]
+
     df.set_index('mapped_phosphosite', inplace=True)
 
-    li = []
+    li_up = []
 
     for i, val in df.iterrows():
         for site in all_sites:
-            c = getcount(val['exp_condition'], df.loc[site, 'exp_condition'])
-            li.append(c)
-        df[i] = li
-        li = []
+            c_up = getcount_up(val['condition_exp'], df.loc[site, 'condition_exp'])
+            li_up.append(c_up)
+        df[i] = li_up
+        li_up = []
     
-    df.drop(['exp_condition'], axis=1, inplace=True)
+    df.drop('condition_exp',axis=1, inplace = True)
+    
+    _dict = {}
+    main_dic = {}
+    
+    for i , row in df.iterrows():
+        came = False
+        for k,v in row.items():
+            if i == k:
+                _dict[k] = sum(v)
+                came = True
+            else:
+                if came:
+                    _dict[k] = v[1]
+                else:
+                    _dict[k] = v[0]
+
+
+        main_dic[i] = _dict
+        _dict = {}
+
+    result  = pd.DataFrame(main_dic)
+
+    result.to_excel("PRPF4B_matrix.xlsx")
+
+
     return df
+
+def sum_up_down(mdf_up,mdf_down):
+    pass
+
+
+
+
+
+
+
+
